c7daf56664ecaf87b2916a34097f9debd640e04e
petro2561 <andreev.pete@gmail.com>
2024-06-03 18:22:43 +0300

    bot_username

--- bot/config.py
+++ bot/config.py
@@ -5,9 +5,8 @@ from environs import Env
 
 @dataclass
 class TgBot:
-    token: str  # Токен для доступа к телеграм-боту
-    admin_ids: list[int] | None  # Список id администраторов бота
-    bot_username: str # Username бота
+    token: str
+    admin_ids: list[int] | None
 
 
 @dataclass
@@ -20,6 +19,6 @@ def load_config(path: str | None = None) -> Config:
     env.read_env(path)
     return Config(
         tg_bot=TgBot(
-            token=env("BOT_TOKEN"), admin_ids=list(map(int, env.list("ADMIN_IDS"))), bot_username=("BOT_USERNAME")
+            token=env("BOT_TOKEN"), admin_ids=list(map(int, env.list("ADMIN_IDS")))
         )
     )
