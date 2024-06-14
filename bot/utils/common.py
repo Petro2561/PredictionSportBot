5ca78d2275c21b472048988446806702844ad611
petro2561 <andreev.pete@gmail.com>
2024-06-14 21:29:29 +0300

    fix

--- bot/utils/common.py
+++ bot/utils/common.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
 from db.db import get_async_session
-from db.models import MatchPrediction, Player, Tournament
+from db.models import Match, MatchPrediction, Player, Tournament
 from db.crud.tour import crud_tour
 
 
@@ -11,14 +11,19 @@ async def get_predictions(player: Player):
     async for session in get_async_session():
         player_with_predictions = await session.execute(
             select(Player)
-            .options(selectinload(Player.match_predictions)
-                    .selectinload(MatchPrediction.match))
-            .where(Player.id == player.id, Player.is_eliminated==False)
+            .options(
+                selectinload(Player.match_predictions)
+                .selectinload(MatchPrediction.match)
+                .selectinload(Match.tour),
+                selectinload(Player.tournament)
+            )
+            .where(Player.id == player.player_id, Player.is_eliminated == False)
         )
         player = player_with_predictions.scalars().first()
         if player:
             for prediction in player.match_predictions:
-                result_message += f"{(prediction.match.first_team)} - {prediction.match.second_team} {prediction.match.first_team_score}-{prediction.second_team_score} Очки: {prediction.points} \n"
+                if prediction.match.tour.id == player.tournament.current_tour_id
+                    result_message += f"{(prediction.match.first_team)} - {prediction.match.second_team} {prediction.first_team_score}-{prediction.second_team_score} Очки: {prediction.points} \n"
         return result_message
 
 async def send_long_message(chat_id: int, message: str, bot: Bot):
